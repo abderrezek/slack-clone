73d8bff21df890c1554fc784b9e6c6072153520a
Abderrezek <galabde94@gmail.com>
2020-10-25 09:51:17 +0100

    Add User item in Menu

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Dropdown, Grid, Header, Icon } from "semantic-ui-react";
+
+const UserPanel = () => {
+  const _dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+          Signed in as <strong>User</strong>
+        </span>
+      ),
+      disabled: true,
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>,
+    },
+    {
+      key: "signout",
+      text: <span>Signed Out</span>,
+    },
+  ];
+
+  return (
+    <Grid style={{ backgroundColor: "#4c3c4c" }}>
+      <Grid.Column>
+        <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
+          {/* App header */}
+          <Header inverted floated="left" as="h2">
+            <Icon name="code" />
+            <Header.Content>DevChat</Header.Content>
+          </Header>
+
+          {/* User Dropdown */}
+          <Header style={{ padding: "0.25em" }} as="h4" inverted>
+            <Dropdown
+              trigger={<span>User</span>}
+              options={_dropdownOptions()}
+            />
+          </Header>
+        </Grid.Row>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default UserPanel;
