f458d41880d05564cc5b7eaf28edef034f7424f9
Abderrezek <galabde94@gmail.com>
2020-10-25 09:50:35 +0100

    Add Menu

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Menu } from "semantic-ui-react";
+
+import UserPanel from "./UserPanel";
 
 const SidePanel = () => {
-  return <div>SidePanel</div>;
+  return (
+    <Menu
+      inverted
+      vertical
+      size="large"
+      fixed="left"
+      style={{ backgroundColor: "#4c3c4c" }}
+    >
+      <UserPanel />
+    </Menu>
+  );
 };
 
 export default SidePanel;
