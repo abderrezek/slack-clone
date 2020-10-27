f1e7d492022e6e4f7785de4748fb10b3f7747be6
Abderrezek <galabde94@gmail.com>
2020-10-27 09:52:58 +0100

    Scaffold Color Panel Components

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button, Divider, Menu, Sidebar } from "semantic-ui-react";
 
 const ColorPanel = () => {
-  return <div>ColorPanel</div>;
+  return (
+    <Sidebar
+      as={Menu}
+      width="very thin"
+      icon="labeled"
+      inverted
+      vertical
+      visible
+    >
+      <Divider />
+      <Button icon="add" size="small" color="blue" />
+    </Sidebar>
+  );
 };
 
 export default ColorPanel;
